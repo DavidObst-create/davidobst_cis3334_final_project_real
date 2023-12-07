ef8fc7fa058d998d3420e1cc376a57ef35c0fae5
jobst <jobst@css.edu>
2023-12-07 11:34:15 -0600

    Started implementation of firebase DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -37,7 +44,7 @@ bool isBotThinking = false;
 
 
 
-  void _incrementCounter() {
+  void createMessage() {
     setState(() {
       messages.add("Hello, I am a chatbot. How can I help you?");
       if (isBotThinking == false) {
@@ -66,19 +73,80 @@ bool isBotThinking = false;
             return ListTile(
               title: Text(messages[position]),
               tileColor: Colors.green,
-
             );
-        },
-
+          },
         )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        floatingActionButton: FloatingActionButton(
+          onPressed: createMessage,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+//------------------------------------------------------------------------------
+//Firebase Code
+//TODO create ListTile method that uses the _newMessageTextField TextEditingController as a controller
+
+class Firebase extends StatefulWidget {
+  //@override
+  //_FirebaseState createState() => _FirebaseState();
+}
+
+class _FirebaseState extends State<Firebase> {
+  final TextEditingController _newMessageTextField = TextEditingController();
+
+  final CollectionReference messageCollectionDB = FirebaseFirestore.instance.collection('MESSAGES');
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+          buildRow(context),
+            SizedBox(height: 40,),
+            ListItemsWidget(messageCollectionDB),
+          ],
+        ),
+      ),
     );
   }
 
+Expanded ListItemsWidget(messageCollectionDB) {
+    return Expanded(
+              child: StreamBuilder(
+                stream: messageCollectionDB.snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (BuildContext context, int position) {
+                      return Card(
+                          child: listTileWidget(snapshot, position),
+                      );
+                    }
+                  );
+                }
+              )
+            );
+  }
+
+  ListTile listTileWidget(AsyncSnapshot<QuerySnapshot> snapshot, int position) {
+    return ListTile(
+      leading: Icon(Icons.check_box),
+      title: Text(snapshot.data!.docs[position]['message']),
+      onTap: () {
+        setState(() {
+          _newMessageTextField.text = snapshot.data!.docs[position]['message'];
+        });
+      },
+    );
+  }
 
 }
